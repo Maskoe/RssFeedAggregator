0928cec2329e20ac3cae79b499e419e68a496cbf
Mirco Benthien <Mirco@Benthien.dev>
2024-03-03 13:12:04 +0100

    AsParameter clean up

--- RssFeedAggregator/Handlers/GetNewestPostsHandler.cs
+++ RssFeedAggregator/Handlers/GetNewestPostsHandler.cs
@@ -7,23 +7,24 @@ namespace MockStuff.Handlers;
 public class GetNewestPostsHandler
 {
     // this works much better in FE, RequestBinding.
-    public static async Task<IResult> Execute(HttpContext httpContext, AppDbContext dbContext, CancellationToken ct)
+    public static async Task<IResult> Execute(
+        [AsParameters] GetNewestPostsRequest req,
+        HttpContext httpContext, 
+        AppDbContext dbContext, 
+        CancellationToken ct)
     {
         var currentUser = httpContext.CurrentUser();
-        var limit = int.TryParse(httpContext.Request.Query["limit"], out var parsedInt) ? parsedInt : 20;
 
-        var user = await dbContext.AppUsers
-            .Include(x => x.Feeds)
-            .ThenInclude(x => x.Posts.Take(limit))
-            .FirstAsync(x => x.Id == currentUser.Id);
-
-        var posts = user.Feeds
-            .SelectMany(x => x.Posts)
+        var posts = await dbContext.Set<Post>()
+            .Where(x => x.Feed.AppUsers.Any(u => u.Id == currentUser.Id))
             .OrderByDescending(x => x.PublishedAt)
-            .Select(x => new PostDto(x.Id, x.Title, x.Description, x.PublishedAt, x.Url, x.Feed.Name));
+            .Take(req.Limit ?? 200)
+            .Select(x => new PostDto(x.Id, x.Title, x.Description, x.PublishedAt, x.Url, x.Feed.Name))
+            .ToListAsync();
         
         return TypedResults.Ok(posts);
     }
 }
 
+public record GetNewestPostsRequest(int? Limit);
 public record PostDto(Guid Id, string Title, string Description, DateTime PublishedAt, string Url, string FeedName);
