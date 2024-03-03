1af70aa2a2d8cbe266764b08994c4a09d5c1f820
Mirco Benthien <Mirco@Benthien.dev>
2024-03-03 12:52:09 +0100

    sync-feeds + newewst-posts

---
+++ RssFeedAggregator/Handlers/GetNewestPostsHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MockStuff.Db;
+
+namespace MockStuff.Handlers;
+
+public class GetNewestPostsHandler
+{
+    // this works much better in FE, RequestBinding.
+    public static async Task<IResult> Execute(HttpContext httpContext, AppDbContext dbContext, CancellationToken ct)
+    {
+        var currentUser = httpContext.CurrentUser();
+        var limit = int.TryParse(httpContext.Request.Query["limit"], out var parsedInt) ? parsedInt : 20;
+
+        var user = await dbContext.AppUsers
+            .Include(x => x.Feeds)
+            .ThenInclude(x => x.Posts.Take(limit))
+            .FirstAsync(x => x.Id == currentUser.Id);
+
+        var posts = user.Feeds
+            .SelectMany(x => x.Posts)
+            .OrderByDescending(x => x.PublishedAt)
+            .Select(x => new PostDto(x.Id, x.Title, x.Description, x.PublishedAt, x.Url, x.Feed.Name));
+        
+        return TypedResults.Ok(posts);
+    }
+}
+
+public record PostDto(Guid Id, string Title, string Description, DateTime PublishedAt, string Url, string FeedName);
--- RssFeedAggregator/Program.cs
+++ RssFeedAggregator/Program.cs
@@ -26,6 +26,7 @@ var sp = app.Services.CreateScope().ServiceProvider;
 var dbContext = sp.GetRequiredService<AppDbContext>();
 await dbContext.Database.MigrateAsync();
 
+
 var v1 = app.MapGroup("v1");
 
 v1.MapPost("/register", CreateUserHandler.Execute);
@@ -37,9 +38,14 @@ v1.MapGet("/feeds", GetFeedsHandler.Execute).AddEndpointFilter<AuthFilter>();
 v1.MapPost("/subscribe-to-feed", SubscribeToFeedHandler.Execute).AddEndpointFilter<AuthFilter>();
 v1.MapPost("/unsubscribe-from-feed", UnsubscribeFromFeedHandler.Execute).AddEndpointFilter<AuthFilter>();
 v1.MapGet("/my-feeds", GetUserFeedsHandler.Execute).WithAuth();
+v1.MapGet("/newest-posts", GetNewestPostsHandler.Execute).WithAuth();
 
-v1.MapPost("/go", FetchFeedsHandler.FetchAll);
 
-// ToDo fetch every 10min
+// Set up background tasks
+var handler = sp.GetRequiredService<FetchFeedsHandler>();
+var recurringJobs = sp.GetRequiredService<IRecurringJobManager>();
+//recurringJobs.AddOrUpdate("sync-feeds-sequentially", () => handler.FetchFeedsSequentially(), Cron.Minutely);
+//recurringJobs.AddOrUpdate("sync-feeds-concurrently", () => handler.FetchFeedsConcurrently(), Cron.Minutely);
+//recurringJobs.AddOrUpdate("100x", () => FetchFeedsHandler.DoSomething100Times(), Cron.Minutely);
 
 app.Run();
